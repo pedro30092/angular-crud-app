e19aff3bba776b8854466e5ac83d71818255b100
Pedro Hernandez <pedro30092@gmail.com>
2025-04-11 09:43:37 -0600

    Used CSS instead of SASS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'crud-app';
+  title = 'angular-crud-app';
 }
