534e432a38f27324e93ac3b264c66949bc133878
Pedro Hernandez <pedro30092@gmail.com>
2025-04-11 10:54:44 -0600

    Added todo components Added dynamic data to components

---
+++ src/app/todos/todo-list-item/todo-list-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-item',
+  imports: [],
+  templateUrl: './todo-list-item.component.html',
+  styleUrl: './todo-list-item.component.css',
+})
+export class TodoListItemComponent {
+  title = input.required<string>();
+  isCompleted = signal<boolean>(false);
+
+  toggleCompleted() {
+    this.isCompleted.update((completed) => !completed);
+  }
+}
